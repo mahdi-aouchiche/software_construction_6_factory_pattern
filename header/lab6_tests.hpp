43a9754f3c483fb0190e8916b75014ac60bbd262
mahdi-aouchiche <mahdiaouchiche2022@gmail.com>
2022-07-18 16:33:05 -0700

    units test to run in lab6 for factry expression tree class

---
+++ header/lab6_tests.hpp
@@ -0,0 +1,7 @@
+#ifndef __LAB6_TESTS_HPP__
+#define __LAB6_TESTS_HPP__
+
+/* add all the test files to run in lab 6 */
+#include "factory_expression_tree_unit_test.hpp"
+
+#endif  //__LAB6_TESTS_HPP__
